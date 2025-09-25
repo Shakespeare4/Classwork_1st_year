e8f70eede727dd7616e3dd20f65df6cc5822c6a7
Mateo Carter <shakesmateocourse@gmail.com>
2025-09-25 18:57:50 +0200

    Added the start of the next project

---
+++ Scripts/Java/Practica2PROUEX01/mateowcsprouex01/src/main/java/es/cide/programacio/Main.java
@@ -0,0 +1,29 @@
+//nom: Mateo William Carter Shakespeare
+//data: 25/09/2025
+//nie: Y1723089-N
+
+package es.cide.programacio;
+
+public class Main {
+    public static void main(String[] args) {
+        int registro1; //es correcte
+        //int 1registro; incorrecte, no pot començar per numero
+        int archivo_3; //es correcte
+        //int while; incorrecte, while es una paraula reservada
+        int $impuesto;
+        //int año; es correcte, pero nomes perque el visual interve en la compilacio
+        //int primer apellido; incorrecte, no pot tenir espais
+        int primer_apellido; //es correcte
+        //int primer-apellido; incorrecte, no pot tenir guio
+        int primerApellido; //es correcte
+        //int Tom's; incorrecte, no pot tenir apostrofes
+        int C3P0; //es correcte
+        //int 123#; incorrecte, no pot tenir coixinets 
+        //int PesoMáximo; es correcte, pero nomes perque el visual interve en la compilacio
+        //int %descuento; incorrecte, no pot tenir percentatges
+        int Weight; // es correcte
+        //int $$precioMínimo;es correcte, pero nomes perque el visual interve en la compilacio
+        //int _$Único; es correcte, pero nomes perque el visual interve en la compilacio
+        //int tamaño_máximo; es correcte, pero nomes perque el visual interve en la compilacio
+    }
+}
