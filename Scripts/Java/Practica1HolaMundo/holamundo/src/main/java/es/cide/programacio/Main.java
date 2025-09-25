d6c2fb7ceabc30b13d37bb15527cdcedbaf1198d
Mateo Carter <shakesmateocourse@gmail.com>
2025-09-25 18:18:39 +0200

    Add comment for clarity in Main.java

--- Scripts/Java/Practica1HolaMundo/holamundo/src/main/java/es/cide/programacio/Main.java
+++ Scripts/Java/Practica1HolaMundo/holamundo/src/main/java/es/cide/programacio/Main.java
@@ -1,5 +1,5 @@
 package es.cide.programacio;
-
+// Hola Mundo en Java
 public class Main {
     public static void main(String[] args) {
         System.out.println(((3+4) == 7) && ! (12.3>2.11) || ( 'a' =='b'));
