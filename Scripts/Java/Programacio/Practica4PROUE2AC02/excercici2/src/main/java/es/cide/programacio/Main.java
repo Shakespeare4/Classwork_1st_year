1b3673eff2937c185f33eb2af8c5a53691a93c79
Mateo Carter <shakesmateocourse@gmail.com>
2025-10-10 16:55:20 +0200

    Refactor Main.java to improve input handling and streamline approval counting logic

--- Scripts/Java/Programacio/Practica4PROUE2AC02/excercici2/src/main/java/es/cide/programacio/Main.java
+++ Scripts/Java/Programacio/Practica4PROUE2AC02/excercici2/src/main/java/es/cide/programacio/Main.java
@@ -7,10 +7,30 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int[] notes = new int[6];
         int i = 0;
+        System.out.println("Introdueix les notes (0-10) de 6 alumnes:");
+        
         while (i < notes.length) {
-            notes[i] = sc.nextInt();
+            int nota = sc.nextInt();
+            if (nota >= 0 && nota <= 10) {
+                notes[i] = nota;
+                i++;
+            } else {
+                System.out.println("Nota no vàlida. Torna a introduir:");
+            }
+        }
+        int aprovats = 0;
+        int suspesos = 0;
+        i = 0;
+        while (i < notes.length) {
+            if (notes[i] >= 5) {
+                aprovats++;
+            } else {
+                suspesos++;
+            }
             i++;
         }
-        System.out.println();
+        System.out.println("Alumnes aprovats: " + aprovats);
+        System.out.println("Alumnes suspesos: " + suspesos);
+        sc.close();
     }
 }
